2e4175e51b57e0434b134a6d174019f0236a60d0
Ph3nol <ph3@slynett.com>
2012-07-24 22:23:32 +0200

    Manager interface updated

--- Manager/ManagerInterface.php
+++ Manager/ManagerInterface.php
@@ -4,4 +4,18 @@ namespace Sly\PushOverBundle\Manager;
 
 interface ManagerInterface
 {
+    /**
+     * Set message (with or without options).
+     * 
+     * @param string $message Message
+     * @param array  $options Options
+     */
+    public function setMessage($message, array $options = array());
+
+    /**
+     * Push message.
+     * 
+     * @return boolean
+     */
+    public function push();
 }
